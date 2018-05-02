a6900cec83a91ba79d79e6cb1c786b15b5a2e7d6
carloscespedes <cespedescarlos0610@gmail.com>
2018-05-02 19:36:52 -0400

    Agregar archivos de proyecto.

---
+++ proyecto0205/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(proyecto0205.Startup))]
+namespace proyecto0205
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
